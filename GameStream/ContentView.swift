080d9d50e9774b00dfd721a379ce5058ac06d191
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-08-22 18:31:28 -0400

    Initial Commit

---
+++ GameStream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by Jackson Cuevas on 22/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Jackson Cuevas on 22/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
