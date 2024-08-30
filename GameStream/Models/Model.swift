90be810d358c0ebf6ae0077d232ad827dbeebb5f
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-08-29 20:11:57 -0400

    Modelando nuestro JSON

--- GameStream/Models/Model.swift
+++ GameStream/Models/Model.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+// MARK: - Game
+struct Game: Codable {
+    let title, studio, contentRaiting, publicationYear: String
+    let description: String
+    let platforms, tags: [String]
+    let videosUrls: VideosUrls
+    let galleryImages: [String]
+}
+
+// MARK: - VideosUrls
+struct VideosUrls: Codable {
+    let mobile, tablet: String
+}
+
+typealias Games = [Game]
