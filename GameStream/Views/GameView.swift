f1528c4caebb68e673d94efbc1fc919b0c793e11
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-05 20:22:38 -0400

    Mostrar alertas

--- GameStream/Views/GameView.swift
+++ GameStream/Views/GameView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import AVKit
+import Kingfisher
 
 struct GameView: View {
     var url: String
@@ -26,6 +27,9 @@ struct GameView: View {
                 video(url: url).frame(height: 300)
                 ScrollView {
                     videoInfo(titulo: titulo, studio: studio, calificacion: calificacion, anoPublicacion: anoPublicacion, descripcion: descripcion, tags: tags)
+                    
+                    Gallery(imgsUrl: imgsUrl)
+                    Comments()
                 }.frame(maxWidth: .infinity)
             }
         }
@@ -61,7 +65,7 @@ struct videoInfo: View {
                     .font(.subheadline)
                     .padding(.top, 5)
                     .padding(.leading)
-            
+                
                 Text("\(calificacion)")
                     .foregroundColor(.white)
                     .font(.subheadline)
@@ -91,6 +95,83 @@ struct videoInfo: View {
             }
             
         }.frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct Gallery: View {
+    var imgsUrl: [String]
+    
+    let formaGrid = [
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("GALERÍA")
+                .foregroundColor(.white)
+                .font(.title)
+                .padding(.leading)
+            
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: formaGrid, spacing: 8) {
+                    ForEach(imgsUrl, id: \.self){
+                        imgUrl in
+                        
+                        KFImage(URL(string: imgUrl))
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    }
+                }
+            }
+            
+            
+        }.frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct Comments: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("COMENTARIOS").foregroundColor(.white).font(.title).padding(.leading)
+            
+            VStack {
+                HStack {
+                    Image("profile")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 80, alignment: .leading).padding(.leading)
+                    
+                    VStack {
+                        Text("Enrique Ramos").foregroundColor(.white).font(.subheadline).fontWeight(.bold).frame(alignment: .leading)
+                        
+                        Text("Hace 7 días").foregroundColor(.white).font(.subheadline).frame(alignment: .leading)
+
+                    }
+                }.frame(maxWidth: .infinity, alignment: .leading).padding(.top, 10)
+                
+                Text("He visto que como media tiene una gran calificación, y estoy completamente de acuerdo. Es el mejor juego que he jugado sin ninguna duda, combina una buena trama con una buenísima experiencia de juego libre gracias a su inmenso mapa y actividades.").foregroundColor(.white).font(.subheadline).frame(maxWidth: 340, alignment: .leading).padding(.bottom, 15)
+                
+            }.frame(maxWidth: .infinity, alignment: .leading).background(Color("blue-gray")).clipShape(RoundedRectangle(cornerRadius: 8)).padding(.all, 10)
+            
+        }
+        VStack {
+            HStack {
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80, height: 80, alignment: .leading).padding(.leading)
+                
+                VStack {
+                    Text("Enrique Ramos").foregroundColor(.white).font(.subheadline).fontWeight(.bold).frame(alignment: .leading)
+                    
+                    Text("Hace 7 días").foregroundColor(.white).font(.subheadline).frame(alignment: .leading)
+
+                }
+            }.frame(maxWidth: .infinity, alignment: .leading).padding(.top, 10)
+            
+            Text("He visto que como media tiene una gran calificación, y estoy completamente de acuerdo. Es el mejor juego que he jugado sin ninguna duda, combina una buena trama con una buenísima experiencia de juego libre gracias a su inmenso mapa y actividades.").foregroundColor(.white).font(.subheadline).frame(maxWidth: 340, alignment: .leading).padding(.bottom, 15)
+            
+        }.frame(maxWidth: .infinity, alignment: .leading).background(Color("blue-gray")).clipShape(RoundedRectangle(cornerRadius: 8)).padding(.all, 10)
     }
 }
 
--- GameStream/Views/Home.swift
+++ GameStream/Views/Home.swift
@@ -50,6 +50,7 @@ struct Home: View {
 
 struct PantallaHome: View {
     @State var textoBusqueda = ""
+    
     var body: some View {
         ZStack {
             Color("marine").ignoresSafeArea()
@@ -57,22 +58,6 @@ struct PantallaHome: View {
             ScrollView {
                 VStack {
                     Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 11.0)
-                    
-                    HStack {
-                        Button(action: busqueda, label: {
-                            Image(systemName: "magnifyingglass").foregroundColor(textoBusqueda.isEmpty ? Color(.yellow) : Color("dark-cian"))                .font(.title3)
-                            
-                        })
-                        
-                        ZStack(alignment: .leading) {
-                            if textoBusqueda.isEmpty {
-                                Text("Buscar un video").foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0))                .font(.title3)
-                                
-                            }
-                            
-                            TextField("", text: $textoBusqueda).foregroundColor(.white)
-                        }
-                    }.padding([.top, .leading, .bottom], 11.0).background(Color("blue-gray")).clipShape(Capsule())
                     SubModuloHome()
                 }.padding(.horizontal, 18)
             }
@@ -82,19 +67,42 @@ struct PantallaHome: View {
         
     }
     
-    func busqueda() {
-        print("buscando.. \(textoBusqueda)")
-    }
+//    func busqueda() {
+//        print("buscando.. \(textoBusqueda)")
+//    }
 }
 
 
 struct SubModuloHome: View {
+    @State var isGameInfoEmpty = false
+    @State var textoBusqueda = ""
     @State var url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4"
     @State var isPlayerActive = false
     let urlVideos:[String] = ["https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256671638/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256720061/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256814567/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256801252/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256757119/movie480.mp4"]
     
     var body: some View {
         VStack {
+            
+            HStack {
+                Button(action: {
+                    watchGame(name: textoBusqueda)
+                }, label: {
+                    Image(systemName: "magnifyingglass").foregroundColor(textoBusqueda.isEmpty ? Color(.yellow) : Color("dark-cian"))                .font(.title3)
+                    
+                }).alert(isPresented: $isGameInfoEmpty) {
+                    Alert(title: Text("Error"), message: Text("No se encontró el juego"), dismissButton: .default(Text("Entendido")))
+                }
+                
+                ZStack(alignment: .leading) {
+                    if textoBusqueda.isEmpty {
+                        Text("Buscar un video").foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0))                .font(.title3)
+                        
+                    }
+                    
+                    TextField("", text: $textoBusqueda).foregroundColor(.white)
+                }
+            }.padding([.top, .leading, .bottom], 11.0).background(Color("blue-gray")).clipShape(Capsule())
+            
             Text("LOS MÁS POPULARES")
                 .font(.title3)
                 .foregroundColor(.white)
@@ -218,11 +226,18 @@ struct SubModuloHome: View {
                     })
                 }
             }
-            
-            
         }
+        
     }
+    
+    func watchGame(name: String) {
+        print("Buscar juego")
+        isGameInfoEmpty = true
+    }
+
 }
+
+
 #Preview {
     Home()
 }
