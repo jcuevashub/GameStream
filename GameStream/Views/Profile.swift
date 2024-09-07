3262e661776f4ede7d163f43fb5a821852a66450
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-07 11:59:17 -0400

    Pantalla de perfil de usuario

--- GameStream/Views/Profile.swift
+++ GameStream/Views/Profile.swift
@@ -8,16 +8,43 @@
 import SwiftUI
 
 struct Profile: View {
+    @State var nombreUsuario = "Lorem"
+    
     var body: some View {
-        Text("Hola desde pantalla Perfil")
-            .navigationBarHidden(true)
-            .navigationBarBackButtonHidden(true)
-            .onAppear(
-                perform: {
-                    print("Primer element del json:")
-                    print("Titulo del primer videojuego del json")
-                }
-            )
+        ZStack {
+            Color("marine").ignoresSafeArea()
+                .navigationBarHidden(true)
+                .navigationBarBackButtonHidden(true)
+            
+            VStack {
+                Text("Perfil")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity, alignment: .center)
+                
+                VStack{
+                    Image("profile").resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 118.0, height: 118.0, alignment: .center)
+                        .clipShape(Circle())
+                    
+                }.padding(EdgeInsets(top: 64, leading: 0, bottom: 32, trailing: 0))
+                
+                Text("Ajustes")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity, alignment: .leading).padding(.leading, 18)
+                
+                ModuloAjustes()
+                
+            }
+        }
+    }
+}
+
+struct ModuloAjustes: View {
+    var body: some View {
+      Text("Hola")
     }
 }
 
