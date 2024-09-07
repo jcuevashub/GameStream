58f278eab6960a77d78f850139eb16f4f691d384
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-07 11:33:16 -0400

    Pantalla de favoritos

--- GameStream/Views/Favorites.swift
+++ GameStream/Views/Favorites.swift
@@ -6,18 +6,46 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct Favorites: View {
+    
+    @ObservedObject var todosLosVideoJuegos = GameViewModel()
+    
     var body: some View {
-        Text("Hola desde pantalla Favoritos")
-            .navigationBarHidden(true)
+        ZStack {
+            Color("marine").ignoresSafeArea()
+            
+            VStack {
+                Text("FAVORITOS")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding(.bottom, 9.0)
+                
+                ScrollView{
+                    ForEach(todosLosVideoJuegos.gamesInfo, id: \.self) {
+                         juego in
+                        
+                        VStack(spacing: 0) {
+                            VideoPlayer(player: AVPlayer(url: URL(string: juego.videosUrls.mobile)!))
+                                .frame(height: 300)
+                            
+                            Text("\(juego.title)")
+                                .font(.title3)
+                                .foregroundColor(Color.white)
+                                .padding()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .background(Color("blue-gray"))
+                                .clipShape(RoundedRectangle(cornerRadius: 3.0))
+                        }
+                    }
+                    
+                }.padding(.bottom, 8)
+                
+            }.padding(.horizontal, 6)
+        }.navigationBarHidden(true)
             .navigationBarBackButtonHidden(true)
-            .onAppear(
-                perform: {
-                    print("Primer element del json:")
-                    print("Titulo del primer videojuego del json")
-                }
-            )
     }
 }
 
