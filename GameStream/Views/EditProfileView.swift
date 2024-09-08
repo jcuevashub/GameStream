ff84d40946d94209b3e42666dc6020ba18709ec5
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-08 19:54:49 -0400

    Módulo de edición de perfil

--- GameStream/Views/EditProfileView.swift
+++ GameStream/Views/EditProfileView.swift
@@ -19,20 +19,98 @@ struct EditProfileView: View {
                             Image("profile").resizable()
                                 .aspectRatio(contentMode: .fill)
                                 .frame(width: 118.0, height: 118.0, alignment: .center)
-                            .clipShape(Circle())
+                                .clipShape(Circle())
                             
                             Image(systemName: "camera").foregroundColor(.white)
                         }
                     })
                 }.padding(.bottom, 18.0)
+                
+                ModuloEditar()
             }
         }
     }
 }
 
 struct ModuloEditar: View {
+    
+    @State var correo: String = ""
+    @State var contrasena: String = ""
+    @State var nombre: String = ""
+    
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        VStack(alignment: .leading) {
+            Text("Correo electrónico").foregroundColor(Color("dark-cian"))
+            
+            ZStack(alignment: .leading) {
+                if correo.isEmpty {
+                    Text("ejemplo@gmail.com")
+                        .font(.caption)
+                        .foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0))
+                }
+                
+                TextField("",text: $correo)
+            }
+            
+            
+            Divider()
+                .frame(height: 1.0)
+                .background(Color("dark-cian"))
+            
+            Text("Contrasena").foregroundColor(.white)
+
+            
+            ZStack(alignment: .leading) {
+                if contrasena.isEmpty {
+                    Text("Introduce tu contrasena")
+                        .font(.caption)
+                        .foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0))
+                }
+                
+                SecureField("",text: $correo).foregroundColor(.white)
+            }
+            
+            
+            Divider()
+                .frame(height: 1.0)
+                .background(Color("dark-cian"))
+            
+            Text("Nombre").foregroundColor(.white)
+
+            
+            ZStack(alignment: .leading) {
+                if nombre.isEmpty {
+                    Text("Introduce tu nombre de usuario")
+                        .font(.caption)
+                        .foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0))
+                }
+                
+                TextField("",text: $correo).foregroundColor(.white)
+            }
+            
+            
+            Divider()
+                .frame(height: 1.0)
+                .background(Color("dark-cian"))
+                .padding(.bottom, 32)
+            
+            Button(action: actualizarDatos, label: {
+                Text("ACTUALIZAR DATOS")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                    .overlay(RoundedRectangle(cornerRadius: 6.0)
+                        .stroke(Color("dark-cian")
+                                ,lineWidth: 1.0)
+                            .shadow(color: .white, radius: 6))
+            })
+            
+        }.padding(.horizontal, 42.0)
+    }
+    
+    func actualizarDatos() {
+        print("Guardando y recuperando datos del usuario")
     }
 }
 
